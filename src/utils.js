90369027002ae01b6a5fea29bf34c4aa11d02f53
t0nylombardi <alombardi.331@gmail.com>
2024-06-03 14:01:10 -0400

    added palidrome test

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,12 @@
 // Your code here
+export const isPalindrome = (word) => {
+  if (word.length === 0) {
+    throw new Error("Input cannot be an empty string");
+  }
+
+  if (!/^[a-zA-Z]*$/.test(word)) {
+    throw new Error("Input must only contain alphabetic characters");
+  }
+
+  return word.toLowerCase() === word.toLowerCase().split("").reverse().join("");
+};
